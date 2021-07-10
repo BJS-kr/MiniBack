6cc57df969b25d2467f0c3f3f0d4111fc7d2760b
moonjinyoung <wlsdud131@gmail.com>
2021-07-10 11:58:29 +0900

    added db schema

---
+++ schemas/Product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const productSchema = new Schema({
+
+    productName: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    imgPath: {
+        type: Array,
+        required: true
+    }
+
+});
+
+module.exports = mongoose.model("Product", productSchema);
---
+++ schemas/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    favorite: {
+        type: Array
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
