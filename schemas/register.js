febcf32dbdbcaa2e0dcb8810ab5625284c4c4297
BJS-kr <78771384+BJS-kr@users.noreply.github.com>
2021-07-14 02:45:11 +0900

    Update register.js

--- schemas/register.js
+++ schemas/register.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const registerSchema = Joi.object({
   userId: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-  username: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+  username: Joi.string().required(),
   password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{4,30}$')),
   re_password: Joi.ref('pw'),
 });
