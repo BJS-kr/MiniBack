4c1836a0414f587206057a05324646692df6de76
BJS-kr <hahajeng1234@gmail.com>
2021-07-14 03:38:49 +0900

    페이지네이션 수정

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,29 @@ router.get('/', async (req, res, next) => {
   pageNum = 0;
   currentPageNum = 1;
   const contents = pagination(currentPageNum, pageNum, last_id);
+  if (contents.length < 1) {
+    last_id = null;
+  } else {
+    last_id = contents[-1]._id;
+  }
   res.json({ contents: contents });
 });
 
 router.get('/:pageNum', async (req, res, next) => {
   const { pageNum } = req.params;
   const contents = pagination(currentPageNum, pageNum, last_id);
-  last_id = contents[-1]._id;
-  currentPageNum = pageNum;
+  if (contents.length > 0) {
+    last_id = contents[-1]._id;
+    currentPageNum = pageNum;
+    res.json({ contents: contents });
+  } else {
+    res.json({ response: '더 이상 게시글이 없습니다.' });
+  }
 
   // const contents = await Product.find()
   //   .sort({ _id: -1 })
   //   .skip(10 * (pageNum - 1))
   //   .limit(10);
-  res.json({ contents: contents });
 });
 
 module.exports = router;
